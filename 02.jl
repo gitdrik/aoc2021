0d838530ce11252f19777eb267edbedcb7fcaa00
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-02 16:00:01 +0100

    Add files via upload

--- 02.jl
+++ 02.jl
@@ -1,18 +1,10 @@
 open("02.txt") do f
     ins = [(l[1], parse(Int, split(l)[2])) for l in eachline(f)]
-    fw, dp = 0, 0
+    x, y, z = 0, 0, 0
     for (c, n) ∈ ins
-        c == 'd' ? dp += n :
-        c == 'u' ? dp -= n :
-        c == 'f' ? fw += n : nothing
+        c == 'd' ? y += n :
+        c == 'u' ? y -= n : (x, z) = (x+n, z+y*n)
     end
-    println("Part 1: ", fw*dp)
-
-    fw, dp, am = 0, 0, 0
-    for (c, n) ∈ ins
-        c == 'd' ? am += n :
-        c == 'u' ? am -= n :
-        c == 'f' ? (fw, dp) = (fw+n, dp+am*n) : nothing
-    end
-    println("Part 2: ", fw*dp)
+    println("Part 1: ", x*y)
+    println("Part 2: ", x*z)
 end
