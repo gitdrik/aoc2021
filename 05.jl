b9e242f86047f85fbad030d0a39bb67b6f1c8e35
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-05 17:31:33 +0100

    Add files via upload

--- 05.jl
+++ 05.jl
@@ -1,7 +1,5 @@
 open("05.txt") do f
-    lines =
-        readlines(f) .|> s->replace(s, " -> " => ",") |>
-        s->split(s, ',') |> s->parse.(Int, s) |> collect
+    lines = [parse.(Int, split(l, r",| -> ")) for l ∈ eachline(f)]
     M = zeros(Int, 1000, 1000)
     for (x1, y1, x2, y2) ∈ lines
         x1 == x2 ? M[x1, y1:sign(y2-y1):y2] .+= 1 :
