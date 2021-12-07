7d9fea75568fda96c96824d9dbda4893a52884e2
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-07 07:53:08 +0100

    Update 07.jl

--- 07.jl
+++ 07.jl
@@ -3,10 +3,8 @@ open("07.txt") do f
     f1, f2 = typemax(Int), typemax(Int)
     for i ∈ minimum(ns):maximum(ns)
         ds = abs.(ns .- i)
-        c1 = sum(ds)
-        f1 = min(f1, c1)
-        c2 = sum((ds.*ds.+ds).÷2)
-        f2 = min(f2, c2)
+        f1 = min(f1, sum(ds))
+        f2 = min(f2, sum((ds.*ds.+ds).÷2))
     end
     println("Part 1: ", f1)
     println("Part 2: ", f2)
