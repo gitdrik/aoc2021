b5794d1aca24570d53d7bca93399912262fd90d8
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-08 19:27:08 +0100

    Add files via upload

--- 08.jl
+++ 08.jl
@@ -1,25 +1,25 @@
 using Bijections
 
 open("08.txt") do f
-    D = [split.(split(s,'|')) for s in eachline(f)]
+    C = [split.(split(s,'|')) for s in eachline(f)]
     p1 = 0
-    for (ss, out) ∈ D
+    for (ss, out) ∈ C
         p1 += sum([length(d) ∈ Set([2,3,4,7]) for d ∈ out])
     end
     println("Part 1: ", p1)
 
     p2 = 0
-    for (ss, out) ∈ D
+    for (ss, out) ∈ C
         decode = Bijection{Set{Char}, Int}()
         charcount = Dict(Set(s) => length(s) for s ∈ ss)
         uniqueseg = Dict(v => k for (k,v) ∈ charcount)
         fives = [k for (k, v) ∈ charcount if v == 5]
         sixes = [k for (k, v) ∈ charcount if v == 6]
         decode[uniqueseg[2]] = 1
-        decode[[s for s ∈ fives if decode(1) ⊆ s][1]] = 3
         decode[uniqueseg[4]] = 4
         decode[uniqueseg[3]] = 7
         decode[uniqueseg[7]] = 8
+        decode[[s for s ∈ fives if decode(1) ⊆ s][1]] = 3
         decode[decode(3) ∪ decode(4)] = 9
         decode[[s for s ∈ sixes if decode(1) ⊈ s][1]] = 6
         decode[[s for s ∈ sixes if s ≠ decode(9) && s ≠ decode(6)][1]] = 0
