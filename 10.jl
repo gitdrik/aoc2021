c33995c9e494cd29d1f4223b2d314f859a8cbc7b
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-10 08:18:51 +0100

    Add files via upload

---
+++ 10.jl
@@ -0,0 +1,33 @@
+function p()
+open("10.txt") do f
+    ls = readlines(f)
+    points = Dict(')'=>3, ']'=> 57, '}' => 1197, '>' => 25137)
+    points2 = Dict('(' => 1, '[' => 2, '{' => 3, '<' => 4)
+    pairs = Dict('('=>')', '['=> ']', '{' => '}', '<' => '>')
+    p1, P2 = 0, []
+    for l ∈ ls
+        S = []
+        bad = false
+        for c ∈ l
+            if c ∈ keys(pairs)
+                push!(S, c)
+            else
+                open = pop!(S)
+                c == pairs[open] && continue
+                p1 += points[c]
+                bad = true
+                break
+            end
+        end
+        if !bad
+            p2 = 0
+            for c ∈ reverse(S)
+                p2 = p2*5 + points2[c]
+            end
+            push!(P2, p2)
+        end
+    end
+    println("Part 1: ", p1)
+    println("Part 2: ", sort(P2)[(length(P2)+1)÷2])
+end
+end
