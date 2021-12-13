0fa1f1768f39d612a7531a247103c7548104dd2a
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-13 11:55:40 +0100

    Add files via upload

--- 13.jl
+++ 13.jl
@@ -10,17 +10,14 @@ open("13.txt") do f
         f, n = ss[end-1], parse(Int, ss[end])
         nP = Set{Tuple{Int,Int}}()
         for (x, y) ∈ P
-            if f == "x"
-                x = x > n ? 2n-x : x
-            else
-                y = y > n ? 2n-y : y
-            end
+            f == "x" && x > n && (x = 2n-x)
+            f == "y" && y > n && (y = 2n-y)
             push!(nP, (x,y))
         end
         P = nP
         i == 1 && println("Part 1: ", length(P))
     end
-    println("Part 2:")
+    println("Part 2: ")
     for y ∈ 0:5
         for x ∈ 0:40
             print((x,y) ∈ P ? '█' : ' ')
