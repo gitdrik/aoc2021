ade7b636022ddd66b36963918059905bb2d2635d
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-14 14:30:48 +0100

    Add files via upload

--- 14.jl
+++ 14.jl
@@ -1,15 +1,18 @@
-using Counters
 open("14.txt") do f
     s = readline(f)
     readline(f)
     d = Dict(l[1:2]=>l[7] for l ∈ eachline(f))
-
-    for i ∈ 1:10
-        ns = ""
+    dn = Dict([[a, b] for (a, b) in d if ('N' in a*b || 'C' in a*b)])
+    for i ∈ 1:40
+        ns = s[1:1]
         for j ∈ 1:length(s)-1
-            ns *= s[j]*d[s[j:j+1]]*s[j+1]
+            s[j:j+1] in keys(d) && (ns *= d[s[j:j+1]]*s[j+1])
         end
-        println(counter(collect(ns)))
         s = ns
+        C = Dict{Char, Int}()
+        for c ∈ collect(ns)
+            c ∈ keys(C) ? C[c] += 1 : C[c] = 1
+        end
+        println(sort([(b,a) for (a,b) in C]))
     end
 end
