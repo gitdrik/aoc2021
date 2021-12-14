9c318f82538393dcae42c44925cd9484ad419073
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-14 07:58:51 +0100

    Update 14.jl

--- 14.jl
+++ 14.jl
@@ -3,10 +3,10 @@ open("14.txt") do f
     readline(f)
     d = Dict{String,String}()
     for l ∈ eachline(f)
-        d[l[1:2]] = l[1]*l[7]*l[2]
+        d[l[1:2]] = l[7]*l[2]
     end
 
-    ns = ""
+    ns = s[1]
     for i ∈ 1:10
         for j ∈ 1:length(s)-1
             ns *= d[s[i:i+1]]
