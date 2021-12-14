aff5a41e94f386516eb3f3886343bdb2b89c1899
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-14 08:46:08 +0100

    Update 14.jl

--- 14.jl
+++ 14.jl
@@ -4,10 +4,10 @@ open("14.txt") do f
     readline(f)
     d = Dict(l[1:2]=>l[7] for l ∈ eachline(f))
 
-    ns = ""
     for i ∈ 1:10
+        ns = ""
         for j ∈ 1:length(s)-1
-            ns *= s[i]*d[s[i:i+1]]*s[i+1]
+            ns *= s[j]*d[s[j:j+1]]*s[j+1]
         end
         println(counter(collect(ns)))
         s = ns
