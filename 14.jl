fe0f45b09b7382483d85200b9b5c2ffb9151de83
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-14 08:42:36 +0100

    Update 14.jl

--- 14.jl
+++ 14.jl
@@ -3,19 +3,14 @@ using Counters
 open("14.txt") do f
     s = readline(f)
     readline(f)
-    d = Dict{String,String}()
-    for l ∈ eachline(f)
-        d[l[1:2]] = l[7]*l[2]
-    end
+    d = Dict(l[1:2]=>l[7] for l ∈ eachline(f))
 
     ns = ""
     for i ∈ 1:10
-        ns = s[1]
         for j ∈ 1:length(s)-1
-            ns *= d[s[i:i+1]]
+            ns *= s[i]*d[s[i:i+1]]*s[i+1]
         end
         println(counter(collect(ns)))
         s = ns
     end
-
 end
