62990f1c8b7639bd4db0352662d60b2c4d38ed89
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-12-15 20:18:04 +0100

    Add files via upload

--- 14.jl
+++ 14.jl
@@ -1,18 +1,37 @@
 open("14.txt") do f
     s = readline(f)
     readline(f)
-    d = Dict(l[1:2]=>l[7] for l ∈ eachline(f))
-    dn = Dict([[a, b] for (a, b) in d if ('N' in a*b || 'C' in a*b)])
-    for i ∈ 1:40
-        ns = s[1:1]
-        for j ∈ 1:length(s)-1
-            s[j:j+1] in keys(d) && (ns *= d[s[j:j+1]]*s[j+1])
-        end
-        s = ns
-        C = Dict{Char, Int}()
-        for c ∈ collect(ns)
-            c ∈ keys(C) ? C[c] += 1 : C[c] = 1
-        end
-        println(sort([(b,a) for (a,b) in C]))
+    split = Dict(l[1:2] => (string(l[1],l[7]), string(l[7],l[2])) for l ∈ eachline(f))
+    pairs = Dict{String, Int}()
+    for i ∈ 1:length(s)-1
+        s[i:i+1] ∈ keys(pairs) ? pairs[s[i:i+1]] +=1 : pairs[s[i:i+1]] = 1
     end
+
+    function poly(pairs, n)
+        n == 0 && return pairs
+        np = Dict{String, Int}()
+        for (p, m) ∈ pairs
+            p1, p2 = split[p]
+            p1 ∈ keys(np) ? np[p1] += m : np[p1] = m
+            p2 ∈ keys(np) ? np[p2] += m : np[p2] = m
+        end
+        poly(np, n-1)
+    end
+
+    function charcount(pairs)
+        cc = Dict{Char,Int}()
+        for (s, n) ∈ pairs
+            s[1] ∈ keys(cc) ? cc[s[1]] += n : cc[s[1]] = n
+        end
+        s[end] ∈ keys(cc) ? cc[s[end]] += 1 : cc[s[end]] = 1
+        return cc
+    end
+
+    pairs = poly(pairs, 10)
+    cc = charcount(pairs)
+    println("Part 1: ", maximum(values(cc))-minimum(values(cc)))
+
+    pairs = poly(pairs, 30)
+    cc = charcount(pairs)
+    println("Part 2: ", maximum(values(cc))-minimum(values(cc)))
 end
