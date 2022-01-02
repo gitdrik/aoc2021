e601850f8ad214e1d1a3691abcd171246538114c
gitdrik <58667795+gitdrik@users.noreply.github.com>
2022-01-02 18:18:11 +0100

    Add files via upload

--- 21.jl
+++ 21.jl
@@ -11,39 +11,38 @@ function dirac()
     end
     println("Part 1: ", minimum(score)*rolls)
 
-    # Number of 3 to 9 points for a roll
-    diracdice = [1, 3, 6, 7, 6, 3, 1]
-    # Number of universes at each position and score
-    # One row per position, one column per score, 0 score in pos 0
-    scoreboards = [zeros(BigInt, 10, 32), zeros(BigInt, 10, 32)]
-    scoreboards[1][4, 1] = 1
-    scoreboards[2][5, 1] = 1
+    diracdice = [1, 3, 6, 7, 6, 3, 1]  # Number of 3 to 9 points for a roll
+    # Scoreboards, one per player, keeping track of number of universes at each position and score.
+    # Position (1-10) in rows, score (0-30) in column (1-31)
+    scoreboard = [zeros(BigInt, 10, 31), zeros(BigInt, 10, 31)]
+    scoreboard[1][4, 1] = 1
+    scoreboard[2][5, 1] = 1
     wins = Array{BigInt}([0, 0])
     for t ∈ Iterators.countfrom(1)
         roller = isodd(t) ? 1 : 2
         other  = isodd(t) ? 2 : 1
-        newboard = zeros(BigInt, 10, 32)
-        for points ∈ 1:21
-            for pos ∈ 1:10
-                n = scoreboards[roller][pos, points]
-                for i ∈ 3:9
-                    npos = mod1(pos+i, 10)
-                    newpoints = points + npos
-                    newboard[npos, newpoints] += n * diracdice[i-2]
-                end
+        # Roller rolls with diracdice, creating 27 new universes from every
+        # previous universe.
+        newboard = zeros(BigInt, 10, 31)
+        for points ∈ 1:21, pos ∈ 1:10
+            n = scoreboard[roller][pos, points]
+            for i ∈ 3:9
+                npos = mod1(pos+i, 10)
+                newpoints = points + npos
+                newboard[npos, newpoints] += n * diracdice[i-2]
             end
         end
-        scoreboards[roller] = newboard
+        scoreboard[roller] = newboard
         # Increse number of universes also in other player
-        scoreboards[other] .*= 27
-        # compute winners
-        rollerwins = sum(scoreboards[roller][:,22:32])
+        scoreboard[other] .*= 27
+        # Count winners
+        rollerwins = sum(scoreboard[roller][:,22:31])
         wins[roller] += rollerwins
-        # Remove loosers
-        scoreboards[other] -= (scoreboards[other] .* rollerwins) ./ sum(scoreboards[roller])
-        # Remove winners
-        scoreboards[roller][:,22:32] .= 0
-        sum(scoreboards[roller])==0 && break
+        # Remove loosers from game
+        scoreboard[other] -= (scoreboard[other] .* rollerwins) .÷ sum(scoreboard[roller])
+        # Remove winners from game
+        scoreboard[roller][:,22:31] .= 0
+        sum(scoreboard[roller])==0 && break
     end
     println("Part 2: ", maximum(wins))
 end
